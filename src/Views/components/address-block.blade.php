00db4db7b1d92299cd3f8d8387d2462170ff3eec
Mario Jose Goes Tarosso <mariojgt2@gmail.com>
2020-07-28 11:50:10 +0100

    Quick fix to work wint the adminui ecomece stuff

--- src/Views/components/address-block.blade.php
+++ src/Views/components/address-block.blade.php
@@ -5,7 +5,9 @@ $countries = cache()->remember('auicountries', now()->addDays(1) , function() {
 $states = cache()->remember('auistates', now()->addDays(1), function() {
     return AdminUI\AdminUIAddress\Models\State::orderBy('name')->get();
 });
+
 @endphp
+
 <div class="address-block">
     <div class="form-group">
         <label for="Country" class="control-label">{{ __('address.country') }}</label><br/>
@@ -19,18 +21,32 @@ $states = cache()->remember('auistates', now()->addDays(1), function() {
                 </option>
             @endforeach
         </select>
+
+        @error('country_id')
+            <span class="badge badge-danger" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+
     </div>
 
     <div class="form-group">
         <label for="Phone" class="control-label">{{ __('address.phone') }}</label>
-        <input class="form-control" name="phone" type="text" value="{{ old('phone', '') }}" placeholder="{{ __('address.phone') }}">
+        <input class="form-control" name="phone" type="text" value="{{ $address->phone ?? old('phone', '') }}" placeholder="{{ __('address.phone') }}">
+
+        @error('phone')
+            <span class="badge badge-danger" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
     </div>
 
     <div class="row lookup-block">
         <div class="col-sm-6">
             <div class="form-group">
                 <label for="Post Code" class="control-label">{{ __('address.postcode') }}</label>
-                <input class="form-control postcode upperCase" name="postcode" type="text" value="{{ old('postcode', '') }}" placeholder="{{ __('address.postcode') }}">
+                <input class="form-control postcode upperCase" name="postcode" type="text"
+                value="{{ $address->postcode ?? old('postcode', '') }}" placeholder="{{ __('address.postcode') }}">
             </div>
         </div>
 
@@ -57,26 +73,51 @@ $states = cache()->remember('auistates', now()->addDays(1), function() {
         <div class="form-group">
             <label for="Address" class="control-label">{{ __('address.address') }}</label>
             <input class="form-control address_field" name="address"
-                type="text" value="{{ old('address', '') }}"
+                type="text" value="{{ $address->address ?? old('address', '') }}"
                 placeholder="{{ __('address.address') }}">
+
+            @error('address')
+                <span class="badge badge-danger" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
+
         <div class="form-group">
             <label for="Address 2" class="control-label">{{ __('address.address-2') }}</label>
             <input class="form-control address_2_field" name="address_2"
-                type="text" value="{{ old('address_2', '') }}"
+                type="text" value="{{ $address->address_2 ?? old('address_2', '') }}"
                 placeholder="{{ __('address.address-2') }}">
+
+            @error('address_2')
+                <span class="badge badge-danger" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
         <div class="form-group">
             <label for="Town" class="control-label">{{ __('address.town') }}</label>
             <input class="form-control town_field" name="town"
-                type="text" value="{{ old('town', '') }}"
+                type="text" value="{{ $address->town ?? old('town', '') }}"
                 placeholder="{{ __('address.town') }}">
+
+            @error('town')
+                <span class="badge badge-danger" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
         <div class="form-group counties">
             <label for="County" class="control-label">{{ __('address.county') }}</label>
             <input class="form-control county_field" name="county"
-                type="text" value="{{ old('county', '') }}"
+                type="text" value="{{ $address->county ?? old('county', '') }}"
                 placeholder="{{ __('address.county') }}">
+
+            @error('county')
+                <span class="badge badge-danger" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
         <div class="form-group states">
             <label for="County" class="control-label">{{ __('address.state') }}</label>
@@ -91,15 +132,16 @@ $states = cache()->remember('auistates', now()->addDays(1), function() {
         <div class="form-group">
             <label for="Postcode" class="control-label">{{ __('address.postcode') }}</label>
             <input class="form-control address-postcode upperCase" name="postcode"
-                type="text" value="{{ old('postcode', '') }}"
+                type="text" value="{{ $address->postcode ?? old('postcode', '') }}"
                 required="required" placeholder="{{ __('address.postcode') }}">
         </div>
 
         <div class="address-notification alert alert-warning p-1">
         </div>
-        <input type="hidden" name="lng" class="lng_field" value="{{ old('lng', '') }}">
-        <input type="hidden" name="lat" class="lat_field" value="{{ old('lat', '') }}">
-        <input type="hidden" name="distance" class="distance_field" value="{{ old('distance', '') }}">
+        <input type="hidden" name="lng" class="lng_field" value="{{ $address->lng ?? old('lng', '') }}">
+        <input type="hidden" name="lat" class="lat_field" value="{{ $address->lat ?? old('lat', '') }}">
+        <input type="hidden" name="distance" class="distance_field"
+        value="{{ $address->distance ?? old('distance', '') }}">
     </div>
 </div>
 
