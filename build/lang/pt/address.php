398a0ef7a1e1b41a7fcb6761fccec19508539349
mario jose goes tarosso <mariojgt2@gmail.com>
2020-09-10 11:40:55 +0100

    languages updated

---
+++ build/lang/pt/address.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'country'        => 'País',
+    'phone'          => 'Número de telefone',
+    'postcode'       => 'Código Postal',
+    'lookup-btn'     => 'Perquisar',
+    'manual-btn'     => 'Manual',
+    'select-address' => 'Selecione um endereço',
+    'address'        => 'Endereço',
+    'address-2'      => 'endereço linha 2',
+    'town'           => 'Cidade',
+    'county'         => 'município',
+    'state'          => 'Estado dos EUA',
+    'message'        => [
+        'error'     => 'Ocorreu um erro. Por favor, tente novamente',
+        'not-found' => 'Esse código postal não retornou resultados',
+    ]
+];
